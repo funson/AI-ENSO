6d6b63d64827ca0ccdb1a284bd2e6d183176d7b8
Mr.Fang <hsfangwei@sina.com>
2025-11-16 13:43:05 +0000

    Add files via upload

--- metrics.py
+++ metrics.py
@@ -1,6 +1,5 @@
 import numpy as np
 import pytorch_ssim
-from skimage.metrics import structural_similarity as cal_ssim
 
 def MAE(pred, true):
     return np.mean(np.abs(pred-true),axis=(0,1)).sum()
@@ -28,24 +27,6 @@ def metric(pred, true,  return_ssim_psnr=False, clip_range=[0, 1]):
 
     return mse, mae,rmse
 
-def metric_TRUE(pred, true, mean, std, return_ssim_psnr=False, clip_range=[0, 1]):
-    pred = pred*std + mean
-    true = true*std + mean
-    mae = MAE(pred, true)
-    mse = MSE(pred, true)
 
-    if return_ssim_psnr:
-        pred = np.maximum(pred, clip_range[0])
-        pred = np.minimum(pred, clip_range[1])
-        ssim, psnr = 0, 0
-        for b in range(pred.shape[0]):
-            for f in range(pred.shape[1]):
-                ssim += cal_ssim(pred[b, f].swapaxes(0, 2), true[b, f].swapaxes(0, 2), multichannel=True)
-                psnr += PSNR(pred[b, f], true[b, f])
-        ssim = ssim / (pred.shape[0] * pred.shape[1])
-        psnr = psnr / (pred.shape[0] * pred.shape[1])
-        return mse, mae, ssim, psnr
-    else:
-        return mse, mae
 
 
--- recorder.py
+++ recorder.py
@@ -1,3 +1,4 @@
+import matplotlib
 import pandas as pd
 import cmaps
 from global_land_mask import globe
@@ -20,6 +21,7 @@ import warnings
 warnings.filterwarnings("ignore")
 plt.rcParams['axes.unicode_minus'] = False
 c1 = cmaps.temp_19lev
+matplotlib.use('TkAgg')
 
 
 # plt.rcParams['font.sans-serif'] = ['SimHei'] #中文支持
@@ -48,11 +50,10 @@ class Recorder:
 
 
 nino34 = [25, 35, 100, 150]
-nino3 = [25, 35, 120, 170]
+nino3 = [25, 35, 120, 180]
 nino4 = [25, 35, 70, 120]
 ido_west = [10, 14, 10, 14]
 ido_east = [10, 12, 18, 22]
-# IOD指数是通过计算印度洋西部（50°E～70°E, 10°S～10°N）与东部（90°E～110°E, 10°S～0°）区域平均海表温度距平之差来定义的。当IOD指数大于0.4时
 
 
 nino_dict = {0: nino34, 1: nino3, 2: nino4, 3: ido_west, 4: ido_east}
@@ -63,8 +64,6 @@ class Pred_True_vision:
     def __init__(self, pred, true, save_plt_path):
         self.pred = pred  # B,T,H,W
         self.true = true
-        # self.lat=np.array([-59.5, -54.5, -49.5, -44.5, -39.5, -34.5, -29.5, -24.5, -19.5, -14.5, -9.5, -4.5, 0.5, 5.5, 10.5, 15.5, 20.5, 25.5, 30.5, 35.5, 40.5, 45.5, 50.5, 55.5])
-        # self.lon=np.array([0, 5, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95, 100, 105, 110, 115, 120, 125, 130, 135, 140, 145, 150, 155, 160, 165, 170, 175, 180, 185, 190, 195, 200, 205, 210, 215, 220, 225, 230, 235, 240, 245, 250, 255, 260, 265, 270, 275, 280, 285, 290, 295, 300, 305, 310, 315, 320, 325, 330, 335, 340, 345, 350, 355])
         self.nino34_region = nino_dict[0]
         self.nino3_region = nino_dict[1]
         self.nino4_region = nino_dict[2]
@@ -74,12 +73,10 @@ class Pred_True_vision:
         self.acc_weight = np.array([1.5] * 4 + [2] * 7 + [3] * 7 + [4] * 6+[5]) * np.log(np.arange(25) + 1)
 
     def score(self, y_pred, y_true):
-        # for pytorch
         pred = y_pred - np.mean(y_pred, axis=0, keepdims=True)
 
         true = y_true - np.mean(y_true, axis=0, keepdims=True)  # (N, 24)
         cor = (pred * true).sum(axis=0) / (np.sqrt(np.sum(pred ** 2, axis=0) * np.sum(true ** 2, axis=0)) + 1e-6)
-        print(cor.shape[0])
         acc = (self.acc_weight[0:cor.shape[0]] * cor).sum()
         rmse = np.sqrt(np.mean((y_pred - y_true) ** 2, axis=0)).sum()
         return 2 / 3. * acc - rmse, rmse
@@ -108,14 +105,13 @@ class Pred_True_vision:
 
         pred = self.pred
         true = self.true
-        B, T, V, H, W = pred.shape
         # pred = pred.reshape(T,V)
         if 'nino' in nino_region:
             pred_array = np.mean(
-                pred[:, :, 2, nino_region_one[0]:nino_region_one[1] + 1, nino_region_one[2]:nino_region_one[3] + 1],
+                pred[:, :,  nino_region_one[0]:nino_region_one[1] + 1, nino_region_one[2]:nino_region_one[3] + 1],
                 axis=(2, 3))
             true_array = np.mean(
-                true[:, :, 2, nino_region_one[0]:nino_region_one[1] + 1, nino_region_one[2]:nino_region_one[3] + 1],
+                true[:, :,  nino_region_one[0]:nino_region_one[1] + 1, nino_region_one[2]:nino_region_one[3] + 1],
                 axis=(2, 3))
 
             pred_list = np.swapaxes(pred_array, 0, 1)
@@ -123,17 +119,17 @@ class Pred_True_vision:
             true_list = np.swapaxes(true_array, 0, 1)
         else:
             pred_array_west = np.mean(
-                pred[:, :, 2, ido_region_west[0]:ido_region_west[1] + 1, ido_region_west[2]:ido_region_west[3] + 1],
+                pred[:, :,  ido_region_west[0]:ido_region_west[1] + 1, ido_region_west[2]:ido_region_west[3] + 1],
                 axis=(2, 3))
             true_array_west = np.mean(
-                true[:, :, 2, ido_region_west[0]:ido_region_west[1] + 1, ido_region_west[2]:ido_region_west[3] + 1],
+                true[:, :, ido_region_west[0]:ido_region_west[1] + 1, ido_region_west[2]:ido_region_west[3] + 1],
                 axis=(2, 3))
 
             pred_array_east = np.mean(
-                pred[:, :, 2, ido_region_east[0]:ido_region_east[1] + 1, ido_region_east[2]:ido_region_east[3] + 1],
+                pred[:, :,  ido_region_east[0]:ido_region_east[1] + 1, ido_region_east[2]:ido_region_east[3] + 1],
                 axis=(2, 3))
             true_array_east = np.mean(
-                true[:, :, 2, ido_region_east[0]:ido_region_east[1] + 1, ido_region_east[2]:ido_region_east[3] + 1],
+                true[:, :, ido_region_east[0]:ido_region_east[1] + 1, ido_region_east[2]:ido_region_east[3] + 1],
                 axis=(2, 3))
 
             pred_array = pred_array_west - pred_array_east
@@ -190,29 +186,24 @@ class test_vision_exp:
         self.save_path = save_path
 
     def reback_show(self, data):
-        # # 设置间隔为1°的经纬度坐标网格，用插值函数得到该坐标网格点的SST值
+        # # Set a latitude and longitude coordinate grid with an interval of 1°, and use the interpolation function to obtain the SST values at the grid points of this coordinate grid.
 
         lat = np.arange(-59.5, 59.5, 5, )
         lon = np.arange(0, 360, 5, )
-        # 以纬度和经度生成网格点坐标矩阵
         xx, yy = np.meshgrid(lon, lat)
         data_one = data.data
-        # 取样本0第0月的SST值
         z = data_one
-        # 采用三次多项式插值，得到z = f(x, y)的函数f
         f = interpolate.interp2d(lon, lat, np.array(z), kind='cubic')
 
         xnew = np.arange(0, 360, 1, )
         ynew = np.arange(-59.5, 60.5, 1, )
 
         znew = f(xnew, ynew)
-        # 判断坐标矩阵上的网格点是否为陆地
 
         lon_grid, lat_grid = np.meshgrid(xnew - 180, ynew)
         is_on_land = globe.is_land(lat_grid, lon_grid)
         is_on_land = np.concatenate([is_on_land[:, xnew >= 180], is_on_land[:, xnew < 180]], axis=1)
 
-        # 同样进行陆地掩膜
         znew[np.squeeze(is_on_land)] = np.nan
         return np.flipud(znew)
 
@@ -251,7 +242,6 @@ class test_vision_exp:
         ynew = np.arange(-59.5, 60.5, 1, )
         lon = xnew
         lat = ynew
-        print(sst.shape)
         time = one_time
 
         def make_map(ax, title, box, xstep, ystep):
@@ -289,10 +279,7 @@ class test_vision_exp:
                          cmap=plt.cm.RdBu_r, extend='both', levels=np.linspace(-3, 3, 40))
 
         cbar = fig.colorbar(mappable=cp, orientation='horizontal', extend='both', pad=0.06, shrink=0.5)
-        # 调整 colorbar 的大小和间距
-        # cbar.ax.tick_params(labelsize=10)  # 调整刻度标签的大小
-        # cbar.fraction = 0.0046  # 控制 colorbar 的大小
-        # cbar.pad = 0.004  # 控制 colorbar 和图形之间的间距
+
 
         ax.set_title('u-v-sst-{}'.format(time))
         ax.set_xlabel('lon')
@@ -302,16 +289,13 @@ class test_vision_exp:
         plt.show()
 
     def make_git(self):
-        # 设置图片文件夹路径和GIF输出路径
         folder_path = self.save_path
         output_path = f'{self.save_path}/output.gif'
 
-        # 获取文件夹中所有图片的路径
         images = [os.path.join(folder_path, file) for file in os.listdir(folder_path) if
                   file.endswith(('.png', '.jpg', '.jpeg'))]
 
-        # 对图片进行排序，确保它们按照正确的顺序
-        images.sort()  # 你可以根据需要添加更复杂的排序逻辑
+        images.sort()
 
         # 读取图片并保存为GIF
         with imageio.get_writer(output_path, mode='I', duration=500) as writer:
@@ -328,117 +312,3 @@ class test_vision_exp:
             true_back = self.reback_show(true[i, 2])
             self.vision_plot(pre_back, true_back, self.vision_time[i], i)
 
-
-if __name__ == '__main__':
-    # vision_time=pd.date_range(start='2015-01-01',end='2016-12-01',freq='MS')
-    # date_list = vision_time.strftime('%Y-%m-%d').tolist()
-    # # print(date_list)
-    path = r'F:\ENSO-Data\code-2\models\smivp\test'
-    #
-    # pred=xr.open_zarr(r'F:\ENSO-Data\GODAS\godas_min.zarr',consolidated=True).sel(time=slice('2015-01-01','2016-12-01'))['godas_data'].fillna(0).values
-    # print(pred.shape)
-    # xx=np.reshape(pred,(1,24,12,24,72))
-    # pred_exp=np.concatenate((xx,xx),axis=0)
-    # true=pred_exp
-
-    # Pred_True_vision测试
-
-    pred = np.load(r'F:\ENSO-Data\code-2\models\smivp\result_data\pred_list.npy')
-    true = np.load(r'F:\ENSO-Data\code-2\models\smivp\result_data\output_list.npy')
-    print(pred.shape)
-    pred_true = Pred_True_vision(pred, true, path)
-    acc, _ = pred_true.pers_nino_region('ido_west', False)
-    # acc=[0.9736321964704332, 0.9434792855756601, 0.915116732790962, 0.881433036376515, 0.84929712832032, 0.8132301872133048, 0.7834861159094366, 0.7630605299326314, 0.7383100852935461, 0.7253610747682323, 0.6859132365718048, 0.6513792207310953, 0.6113214207360043, 0.5952699069588303, 0.57524611008731, 0.570729147589011, 0.5556539222439761, 0.5155572148806791, 0.5155662407745736, 0.5316992424433764, 0.5362978221620898, 0.5455812514487595, 0.5475807538939721, 0.5517100343473169]
-    #
-    #
-    # # print(acc)
-    # # def trainPlot(self,ACCList, iter, save_root,model_name,nino_region):
-    pred_true.trainPlot(acc, 1, path, 'xx', 'ido_east')
-
-    # test_vision_exp测试
-    # vision=test_vision_exp(date_list,path)
-    # vision.make_plt(pred,true)
-    # # vision.visualize_u_v_sst(true[1],date_list[0])
-    # vision.make_git()
-    #
-    # pre_val=np.load(r'/code/models/smivp/results/Debug/results/Debug/sv/preds.npy')
-    # true_val=np.load(r'/code/models/smivp/results/Debug/results/Debug/sv/trues.npy')
-    #
-    # plt.imshow(pre_val[0,0,2,],cmap=c1)
-    # plt.show()
-    # print(pre_val.shape)
-    # nino_region={0:[]}
-    # val_cl=Pred_True_vision(pre_val,true_val,)
-
-    # # 测试数据集中各区域
-    # import matplotlib.pyplot as plt
-    # import matplotlib.patches as patches
-    # pred=xr.open_zarr(r'F:\ENSO-Data\GODAS\godas_min.zarr',consolidated=True).sel(time=slice('2015-01-01','2016-12-01'))['godas_data']
-    # lat=pred.coords['lat'].values
-    # lon=pred.coords['lon'].values
-    # print(lon.tolist())
-    # print(lat.tolist())
-    # nino34=[-4.5,5.5,190,240]
-    # nino_4=[-4.5,5.5,210,270]
-    # nino_3=[-4.5,5.5,160,210]
-    # # IOD指数是通过计算印度洋西部（50°E～70°E, 10°S～10°N）与东部（90°E～110°E, 10°S～0°）区域平均海表温度距平之差来定义的。当IOD指数大于0
-    # # .4
-    # # 时
-    # ido_weat=[-9.5,10.5,50,70]
-    # ido_east=[-9.5,0.5,90,110]
-    # nino_dict={0:nino34,1:nino_3,2:nino_4,3:ido_weat,4:ido_east}
-    # # print(nino34[0:2])
-    #
-    #
-    # for i in range(len(nino_dict)):
-    #     lat_index = [np.where(lat == nino_dict[i][0])[0], np.where(lat == nino_dict[i][1])[0]]
-    #     lon_index = [np.where(lon == nino_dict[i][2])[0], np.where(lon == nino_dict[i][3])[0]]
-    #
-    #     print(f'lat_nino34: {lat_index}; lon_nino34: {lon_index}')
-
-    # nino34 = [-4.5, 5.5, 190, 240]
-    # nino_4 = [-4.5, 5.5, 210, 270]
-    # nino_3 = [-4.5, 5.5, 160, 210]
-    # nino_dict = {0: nino34, 1: nino_3, 2: nino_4}
-
-    # # 创建一个图表和轴对象
-    # fig, ax = plt.subplots()
-    #
-    # # 设置矩形的左上角和右下角坐标
-    # left_top = (lon_nino34[0][0],lat_nino34[1][0])  # 左上角坐标，范围是0到1，代表相对位置
-    # right_bottom = (12, 14)  # 右下角坐标，范围是0到1
-    #
-    # # 创建一个矩形对象
-    # rect = patches.Rectangle(left_top,10 ,2 , linewidth=1,
-    #                          edgecolor='b', facecolor='none')
-    #
-    # # 将矩形添加到图表中
-    # ax.add_patch(rect)
-    #
-    # # 设置图表的显示范围
-    # ax.imshow(pre_val[0,0,2,],cmap=c1)
-    #
-    #
-    # # 显示图表
-    # plt.show()
-
-    # import matplotlib.pyplot as plt
-    #
-    # # 示例数据
-    # x = [1, 2, 3, 4, 5]
-    # y = [2, 3, 5, 7, 11]
-    #
-    # # 绘制折线图
-    # plt.plot(x, y, marker='o')  # marker='o'表示用圆圈标记数据点
-    #
-    # # # 在每个点旁边显示数值
-    # for i in range(len(x)):
-    #     plt.text(x[i], y[i], str(y[i]), ha='center', va='bottom')  # ha和va控制文本的水平和垂直对齐方式
-    #
-    # # 设置图表标题和坐标轴标签
-    # plt.title('折线图示例')
-    # plt.xlabel('X轴')
-    # plt.ylabel('Y轴')
-    #
-    # # 显示图表
-    # plt.show()
